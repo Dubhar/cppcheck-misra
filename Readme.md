792ac184bc0e30161728c1757a57ea64b0a10c79
Thorsten Jerosch <t.jerosch@kungf.ooo>
2025-01-23 13:59:24 +0100

    enable usage of custom rules.txt

--- Readme.md
+++ Readme.md
@@ -8,3 +8,9 @@ A docker container that utilizes cppcheck's addon to check sourcecode for MISRA 
 docker build -t cppcheck-misra:latest .
 docker run --rm -v "$(pwd)/src:/src" -w /src cppcheck-misra:latest 
 ```
+
+Using custom `rules.txt`
+
+```shell
+docker run --rm -v "$(pwd)/src:/src" -w /src -e RULES_PATH="/src/rules.txt" cppcheck-misra:latest 
+```
--- misra_compliance.sh
+++ misra_compliance.sh
@@ -4,4 +4,5 @@ echo "## Creating CPPCHECK dump ##"
 "${CPPCHECK_INSTALL}"/bin/cppcheck --dump --quiet /src
 
 echo "## Checking dump for MISRA compliance ##"
-python "${CPPCHECK_REPO}/addons/misra.py" --rule-texts="${CPPCHECK_REPO}/addons/test/misra/misra_rules_dummy.txt" /src/*.dump
+RULES_PATH="${RULES_PATH:-${CPPCHECK_REPO}/addons/test/misra/misra_rules_dummy.txt}"
+python "${CPPCHECK_REPO}/addons/misra.py" --rule-texts="${RULES_PATH}" /src/*.dump
